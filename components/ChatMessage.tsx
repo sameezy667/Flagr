a5d8637a08c20d54fa7fd14a66ab4069a12522d5
sameer dhir <93440679+sameezy667@users.noreply.github.com>
2025-07-10 02:18:07 +0530

    debug

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -4,7 +4,7 @@ import { FlagrIcon, UserIcon, CopyIcon, CheckIcon, SpeakerIcon } from '../consta
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { dark } from 'react-syntax-highlighter/styles/prism';
 import { speakText, stopSpeaking } from '../services/speechService';
 
 interface ChatMessageProps {
